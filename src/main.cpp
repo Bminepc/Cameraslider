15cf3e26265223471cfb862c8d5a27470a7b8903
Bminepc <benjamin.wen@outlook.com>
2022-08-12 21:10:10 +0200

    Just some simple Tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <Stepper.h>
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("Hello");
+  delay(1000);
 }
