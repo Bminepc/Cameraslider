b4628cfdc75af65a96f769bd96a63fea9a59bf8d
Bminepc <benjamin.wen@outlook.com>
2022-08-12 22:51:11 +0200

    Slowdown and Speedup

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@ AH_EasyDriver stepperCamera(200,14,12,27,26,28);
 AH_EasyDriver stepperSlider(200,32,33,27,26,28);
 
 int direction = 1;
+int nextDirection = 1;
 int rotationSlider = 5;
 int rotationCamera = 1;
-float afterTurn = 1;
+float speedFactor = 1;
+Boolean hit = false;
 
 void setup(){
   //Steppermotorsetup
@@ -24,26 +26,51 @@ void setup(){
 }
 
 void loop(){
+  if(digitalRead(TasterLeft) == HIGH){
+    hit = true;
+    nextDirection = 1;
+  }else if(digitalRead(TasterRight) == HIGH){
+    hit = true;
+    nextDirection = -1;
+  }
+  if(hit && speedFactor >= 0.1){
+    speedFactor = speedFactor - 0.005;
+  }else if(hit){
+    direction = nextDirection;
+    hit = false;
+  }else if(speedFactor < 1){
+    speedFactor = speedFactor + 0.005;
+  }
+
+
+  stepperCamera.setSpeedRPM(100 * speedFactor);
+  stepperSlider.setSpeedRPM(100 * speedFactor);
+  stepperCamera.rotate(direction * rotationCamera);
+  stepperSlider.rotate(direction * rotationSlider);
+}
+
+/*
+void loop(){
     if(digitalRead(TasterLeft) == HIGH){
       direction = 1;
-      afterTurn = 0.1;
+      speedFactor = 0.1;
     }else if(digitalRead(TasterRight) == HIGH){
       direction = -1;
-      afterTurn = 0.1;
+      speedFactor = 0.1;
     }
 
-    stepperCamera.setSpeedRPM(100 * afterTurn);
-    stepperSlider.setSpeedRPM(100 * afterTurn);
+    stepperCamera.setSpeedRPM(100 * speedFactor);
+    stepperSlider.setSpeedRPM(100 * speedFactor);
     stepperCamera.rotate(direction * rotationCamera);
     stepperSlider.rotate(direction * rotationSlider);
 
-    if(afterTurn < 1){
-      afterTurn = afterTurn + 0.005;
-      Serial.println(afterTurn);
+    if(speedFactor < 1){
+      speedFactor = speedFactor + 0.005;
+      Serial.println(speedFactor);
     }
     delay(1);
 }
-
+*/
 
 
 /*
