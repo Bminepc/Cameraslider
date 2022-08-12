17c5a7259f548a4cdaa1ce4ed5eaece1c9621aaa
Bminepc <benjamin.wen@outlook.com>
2022-08-12 22:00:33 +0200

    Removed more Trash

--- src/old.cpp
+++ src/old.cpp
@@ -2,13 +2,10 @@
 AH_EasyDriver stepperH(200,14,12,27,26,28);
 AH_EasyDriver stepperV(200,32,33,27,26,28);
 void setup(){
-  stepperH.resetDriver();
   stepperH.enableDriver();
-  stepperH.setMicrostepping(0);
   Serial.begin(9600);
   stepperH.setSpeedRPM(100);
   stepperV.setSpeedRPM(100);
-  pinMode(17,INPUT);
 }
 
 void loop(){
